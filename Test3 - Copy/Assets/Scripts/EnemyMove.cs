8ac6dc1e3b53394c3b6ddcf8d4524242217bcca5
makoto91 <veljkovic.matija@gmail.com>
2019-01-22 16:01:22 +0100

    First commit

---
+++ Test3 - Copy/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+public class EnemyMove : MonoBehaviour {
+
+    [SerializeField] Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(player);
+    }
+
+    // Use this for initialization
+    void Start () {
+        //anim.GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!GameManager.instance.GameOver)
+        {
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+            anim.Play("Idle;");
+        }
+        
+	}
+}
